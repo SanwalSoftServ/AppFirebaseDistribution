cc6f200d1e8970f0711ed2174454081d2122fdf2
SanwalSoftServ <106134563+SanwalSoftServ@users.noreply.github.com>
2023-12-22 16:38:43 +0530

    add file 22-12-2023

--- app/src/main/java/com/shree/appfirebasedistribution/MainActivity.java
+++ app/src/main/java/com/shree/appfirebasedistribution/MainActivity.java
@@ -3,12 +3,25 @@ package com.shree.appfirebasedistribution;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView textId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textId = findViewById(R.id.text_id);
+
+        if (BuildConfig.DEBUG) {
+            textId.setText("This is Debug APK");
+        }else{
+            textId.setText("This is Release APK");
+        }
+
     }
 }
